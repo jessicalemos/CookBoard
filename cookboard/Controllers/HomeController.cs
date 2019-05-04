7ecc7656f2cf98ef0408568d7f77588388440da5
Jéssica Lemos <jessica.andreia96@gmail.com>
2019-05-04 13:28:14 +0100

    Bugs & login controller

--- cookboard/Controllers/HomeController.cs
+++ cookboard/Controllers/HomeController.cs
@@ -19,11 +19,5 @@ namespace cookboard.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }

a75af8a096bf39590aac5f04827f3c0ad2ba4dd4
Jéssica Lemos <jessica.andreia96@gmail.com>
2019-05-05 11:04:15 +0100

    Projeto com models

---
+++ cookboard/cookboard/Models/ReceitaReceitaAuxiliar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cookboard.Models
+{
+    public partial class ReceitaReceitaAuxiliar
+    {
+        public int ReceitaId { get; set; }
+        public string ReceitaAuxiliarNome { get; set; }
+        public int Passo { get; set; }
+
+        public virtual Receita Receita { get; set; }
+        public virtual ReceitaAuxiliar ReceitaAuxiliarNomeNavigation { get; set; }
+    }
+}
---
+++ cookboard/cookboard/Models/Supermercado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cookboard.Models
+{
+    public partial class Supermercado
+    {
+        public Supermercado()
+        {
+            SupermercadoLocal = new HashSet<SupermercadoLocal>();
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public virtual ICollection<SupermercadoLocal> SupermercadoLocal { get; set; }
+    }
+}

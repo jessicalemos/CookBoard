b3f15983a2d77992c1c678e2e9587884ac2f243a
Jéssica Lemos <jessica.andreia96@gmail.com>
2019-05-16 17:46:24 +0100

    Inicio dos passos

--- cookboard/cookboard/Controllers/ReceitasController.cs
+++ cookboard/cookboard/Controllers/ReceitasController.cs
@@ -33,11 +33,44 @@ namespace cookboard.Controllers
                                    where (ri.ReceitaId == idReceita)
                                    select i).ToList();
 
+            List<string> quant = (from ri in co.ReceitaIngrediente
+                                  where (ri.ReceitaId == idReceita)
+                                  select ri.Quantidade).ToList();
+
+            List<IngredienteViewModel> final = new List<IngredienteViewModel>();
+            int num = ing.Count();
+
+            for (int i = 0; i < num; i++)
+            {
+                Ingrediente ingrediente = ing[i];
+                string quantidade = quant[i];
+
+                final.Add(new IngredienteViewModel(quantidade, ingrediente));
+            }
+
             Receita rec = (from n in co.Receita
                        where (n.Id == idReceita)
                        select n).Single();
           
-            return View(new ReceitaViewModel(rec,ing));
+            return View(new ReceitaViewModel(rec, final));
+        }
+
+        public ActionResult getPassos(int idReceita)
+        {
+            Receita rec = (from n in co.Receita
+                           where (n.Id == idReceita)
+                           select n).Single();
+
+            List<PassosViewModel> passos = new List<PassosViewModel>();
+            string[] words = rec.Descricao.Split('.');
+            int i = 0;
+            foreach(var word in words)
+            {
+                passos.Add(new PassosViewModel(i, word));
+                i++;
+            }
+
+            return View(passos);
         }
     }
 }
---
+++ cookboard/cookboard/Models/PassosViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cookboard.Models
+{
+    public class PassosViewModel
+    {
+        public PassosViewModel(int numPasso, string passo)
+        {
+            NumPasso = numPasso;
+            Passo = passo;
+        }
+
+        public int NumPasso { get; set; }
+        public string Passo { get; set; }
+    }
+}
